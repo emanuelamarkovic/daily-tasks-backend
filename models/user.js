50410a248b28db87872994faa71a106a9a2f17f0
emanuela <emanuela.markovic@hotmail.com>
2024-04-25 10:51:27 +0200

    feature userSchema

---
+++ models/user.js
@@ -0,0 +1,34 @@
+import { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "you have to enter the name!"],
+    minlength: [3, "name must be at least 3 characters!"],
+  },
+  email: {
+    type: String,
+    required: [true, "you have to enter the email!"],
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid email!`,
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "you have to enter the password!"],
+    minlength: [8, "password must be at least 6 characters!"],
+  },
+  todos: [{ type: Schema.Types.ObjectId, ref: "Todo" }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = model("User", userSchema);
+
+export default User;
