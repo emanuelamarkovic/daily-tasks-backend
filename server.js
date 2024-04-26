9aa817790112c699c52454963ee3a0e2b3877797
emanuela <emanuela.markovic@hotmail.com>
2024-04-26 11:40:34 +0200

    feature: cors and allowed origin

--- server.js
+++ server.js
@@ -2,15 +2,24 @@ import "./config.js";
 import express from "express";
 import "./db-connect.js";
 import logEndPoints from "./utils/logEndpoints.js";
+import cors from "cors";
 
 const port = process.env.PORT;
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const allowedOrigins = [
+  "http://localhost:19006",
+  "https://daily-tasks-app-my36.onrender.com/",
+];
+if (allowedOrigins.includes(origin) || !origin) {
+  callback(null, true);
+} else {
+  callback(new Error("Not allowed by CORS"));
+}
+
+app.use(cors(corsOptions));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
