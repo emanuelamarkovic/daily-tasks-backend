7538af1ddb7bf7c2cb62aac2949cc1010dd5e83b
emanuela <emanuela.markovic@hotmail.com>
2024-05-02 14:29:04 +0200

    feature: update nodemailer configuration and sendPasswordResetEmail function

---
+++ services/emailService.js
@@ -0,0 +1,32 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "hotmail",
+  auth: {
+    user: "emanuela.markovic@hotmail.com",
+    pass: "24031986",
+  },
+});
+
+const sendPasswordResetEmail = async (recipientEmail, resetToken) => {
+  const mailOptions = {
+    from: "emanuela.markovic@hotmail.com",
+    to: recipientEmail,
+    subject: "Password Reset for Daily Tasks",
+    html: ` <p>Hi there,</p>
+    <p>We received a request to reset your password. Please follow the link below to reset your password:</p>
+    <a href="https://daily-tasks-app-my36.onrender.com/reset-password/${resetToken}" >Reset Password</a>
+    <p>If you did not request this password reset, you can safely ignore this email.</p>
+    <p>Best regards,<br/>Your Daily Tasks Team</p>`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Password reset email sent successfully.");
+  } catch (error) {
+    console.error("Error sending password reset email:", error);
+    throw new Error("Failed to send password reset email.");
+  }
+};
+
+export { sendPasswordResetEmail };
