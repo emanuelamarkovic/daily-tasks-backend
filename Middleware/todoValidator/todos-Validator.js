b5911ba04c05c1ec996c08fdeaca2e35cd0b998a
atef0212 <126253950+atef0212@users.noreply.github.com>
2024-05-02 13:43:24 +0200

    feature: Express-Validator is created

---
+++ Middleware/todoValidator/todos-Validator.js
@@ -0,0 +1,27 @@
+import { body, validationResult } from "express-validator";
+
+const toDoValidator = 
+     [
+      body('title').notEmpty().withMessage('Title is required').notEmpty(),
+      body('completed').optional().isBoolean(),
+    
+    ];
+
+
+
+
+
+
+
+
+const validate = (req, res, next) => {
+
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+      return  res.status(422).json({ errors: errors.array() });
+    }
+  next()
+
+  };
+  export {validate, toDoValidator}
---
+++ Middleware/userValidator/user-Validator.js
@@ -0,0 +1,32 @@
+import { body, validationResult } from "express-validator";
+
+
+const userValidator = 
+     [
+        body('email').isEmail().withMessage('Invalid email').notEmpty(),
+
+        body('name').notEmpty().isLength({ min: 3 }).withMessage('Name is required').notEmpty(),
+
+        body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters').notEmpty().matchess(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/, "i"),
+  
+    
+    ];
+
+
+
+
+
+
+
+
+const validate = (req, res, next) => {
+
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+      return  res.status(422).json({ errors: errors.array() });
+    }
+  next()
+
+  };
+  export {validate, userValidator}
