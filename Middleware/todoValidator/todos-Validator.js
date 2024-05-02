75cb876ba0e6386468ad2471368c7acb3bfa419f
atef0212 <126253950+atef0212@users.noreply.github.com>
2024-05-02 13:48:03 +0200

    feature: express-Validator updated

--- Middleware/todoValidator/todos-Validator.js
+++ Middleware/todoValidator/todos-Validator.js
@@ -4,8 +4,19 @@ const toDoValidator =
      [
       body('title').notEmpty().withMessage('Title is required').notEmpty(),
       body('completed').optional().isBoolean(),
+];
+
+    const validate = (req, res, next) => {
+
+        const errors = validationResult(req);
+        console.log(errors);
+        if (!errors.isEmpty()) {
+          return  res.status(422).json({ errors: errors.array() });
+        }
+      next()
     
-    ];
+      };
+      export {validate, toDoValidator}
 
 
 
@@ -14,14 +25,3 @@ const toDoValidator =
 
 
 
-const validate = (req, res, next) => {
-
-    const errors = validationResult(req);
-    console.log(errors);
-    if (!errors.isEmpty()) {
-      return  res.status(422).json({ errors: errors.array() });
-    }
-  next()
-
-  };
-  export {validate, toDoValidator}
