ccf0d8656169ae44f5c2931494a8c7975a8e2c21
emanuela <emanuela.markovic@hotmail.com>
2024-05-07 16:07:15 +0200

    feature: reset-password userRouter

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,7 @@ import {
   signup,
   login,
   forgotPassword,
+  resetPassword,
   logout,
 } from "../controllers/userController.js";
 
@@ -15,6 +16,7 @@ const userRouter = express.Router();
 userRouter.post("/signup", userValidator, validate, signup);
 userRouter.post("/login", userValidator, validate, login);
 userRouter.post("/forgot-password", forgotPassword);
+userRouter.post("/reset-password/:userId/:token", resetPassword);
 userRouter.post("/logout", logout);
 
 export default userRouter;
