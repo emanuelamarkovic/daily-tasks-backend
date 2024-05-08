e12348e10de416b6bd4b40e582e9e1482015015e
emanuela <emanuela.markovic@hotmail.com>
2024-05-08 15:39:23 +0200

    feature: routes tokenvalid and auth user data

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,12 +4,15 @@ import {
   userValidator,
 } from "../Middleware/userValidator/user-Validator.js";
 import {
+  getAuthUser,
   signup,
   login,
   forgotPassword,
   resetPassword,
   logout,
 } from "../controllers/userController.js";
+import { authenticate } from "../Middleware/userValidator/authenticate.js";
+import { tokenValid } from "../Middleware/userValidator/tokenValid.js";
 
 const userRouter = express.Router();
 
@@ -18,5 +21,6 @@ userRouter.post("/login", userValidator, validate, login);
 userRouter.post("/forgot-password", forgotPassword);
 userRouter.post("/reset-password/:userId/:token", resetPassword);
 userRouter.post("/logout", logout);
-
+userRouter.get("/token-valid", tokenValid);
+userRouter.post("/auth-user-data").get(authenticate, getAuthUser);
 export default userRouter;
